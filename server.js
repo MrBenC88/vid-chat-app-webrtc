3a30e937dafaf99585a8bb06208ecce75c953549
Ben Cheung <chessben88@gmail.com>
2020-09-16 22:04:47 -0700

    edit port

--- server.js
+++ server.js
@@ -28,5 +28,3 @@ io.on("connection", (socket) => {
 });
 
 server.listen(process.env.PORT || 3000);
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server Started on ${PORT}`));
