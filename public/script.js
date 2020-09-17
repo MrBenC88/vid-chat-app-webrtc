14e9317bace3a77cd6570c78f6318f31e3989270
Ben Cheung <chessben88@gmail.com>
2020-09-16 21:12:00 -0700

    update

--- public/script.js
+++ public/script.js
@@ -2,7 +2,8 @@ const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   host: "/",
-  port: "3001",
+  path: "/peerjs",
+  port: "443",
 });
 
 const myVideo = document.createElement("video");
