6c261905e8f756dcf1a7f821a3690497b2187e79
Ben Cheung <chessben88@gmail.com>
2020-09-16 22:23:22 -0700

    edited port config

--- public/script.js
+++ public/script.js
@@ -1,8 +1,9 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-  host: "/",
-  port: "3001",
+  host: "peerjs-server.herokuapp.com",
+  secure: true,
+  port: 443,
 });
 
 const myVideo = document.createElement("video");
